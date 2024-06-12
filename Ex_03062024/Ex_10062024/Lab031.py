792e1f1b714e7a1a46fe903357ddf7fd28e1788a
Alviashwini <alviashwini@gmail.com>
2024-06-12 18:40:03 -0500

    Adding area of circle file

--- Ex_03062024/Ex_10062024/Lab031.py
+++ Ex_03062024/Ex_10062024/Lab031.py
@@ -1,3 +1,15 @@
 # Program calculate area of circle
+# Data types
+# input -->  radius --> int or float
+# output --> area --> float
+# core logic --> pi*r*r= 3.14
+radius = float(input("Enter the radius\n"))
 import math
+print(math.pi)
+area = math.pi * (radius ** 2)
+print(area)
+
+# Other way in one line
+import math
+
 print(math.pi * (float(input("Enter the radius\n")) ** 2))
