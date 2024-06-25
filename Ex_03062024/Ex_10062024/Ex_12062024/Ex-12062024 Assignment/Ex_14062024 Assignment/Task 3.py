c38616e1e0a64e6499eca5713e2f5dcb6339929a
Alviashwini <alviashwini@gmail.com>
2024-06-25 01:46:58 -0500

    Assignment

--- Ex_03062024/Ex_10062024/Ex_12062024/Ex-12062024 Assignment/Ex_14062024 Assignment/Task 3.py
+++ Ex_03062024/Ex_10062024/Ex_12062024/Ex-12062024 Assignment/Ex_14062024 Assignment/Task 3.py
@@ -5,6 +5,23 @@
 # 4! -> 4*3*2*1 -> 24
 factorial = int(input("Enter the Number:\n"))
 num = 1
-for i in range(1, factorial+1):
-    num = num*i
+for i in range(1, factorial + 1):
+    num = num * i
 print(f"The Factorial of {factorial} is {num}")
+# or
+n = 5
+factorial = 1
+for i in range(1, n + 1):
+    factorial = factorial * i
+
+print(factorial)
+# or we can use while loop also
+n = 5
+factorial = 1
+while n > 0:
+    factorial = factorial * n
+    n = n - 1
+
+print(factorial)
+
+
--- Ex_03062024/Ex_10062024/Ex_12062024/Ex-12062024 Assignment/Ex_14062024 Assignment/Task1.py
+++ Ex_03062024/Ex_10062024/Ex_12062024/Ex-12062024 Assignment/Ex_14062024 Assignment/Task1.py
@@ -1,15 +1,16 @@
 # Create a program that determines whether a given year is a leap year.
-# A leap year is divisible by 4, but not by 100 unless it is also divisible by 400.
+# A leap year is divisible by 4,
+# but not by 100 unless it is also divisible by 400.
 # Use an if-else statement to make this determination.
 # output is Leapyear
 # input = 2024
+year = 2024
+(year % 4 == 0)
+(year % 100 != 0)
+(year % 400 == 0)
 
-year = int(input("Enter year:\n"))
 
-if year%4 == 0 or year%400 == 0:
-print("leap year")
-elif year % 100 ! = 0:
-
-    print("not a leap year")
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("leap year")
 else:
-    print("invalid entry")
+    print("not a leap year")
